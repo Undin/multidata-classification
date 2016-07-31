2c519035d76df85e4285ef55a047a75766b18103
Arseniy Pendryak <warrior@yandex-team.ru>
2016-07-31 19:34:59 +0300

    calculate macro recall too

--- model/src/main/kotlin/com/warrior/multidata/classification/model/train.kt
+++ model/src/main/kotlin/com/warrior/multidata/classification/model/train.kt
@@ -1,6 +1,10 @@
 package com.warrior.multidata.classification.model
 
+import com.fasterxml.jackson.core.JsonEncoding
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
 import kotlinx.support.jdk8.collections.parallelStream
+import kotlinx.support.jdk8.streams.toList
 import weka.classifiers.Classifier
 import weka.classifiers.evaluation.Evaluation
 import weka.classifiers.meta.FilteredClassifier
@@ -10,149 +14,246 @@ import weka.classifiers.trees.RandomForest
 import weka.core.Instances
 import weka.filters.Filter
 import weka.filters.MultiFilter
+import weka.filters.supervised.instance.SMOTE
 import weka.filters.unsupervised.attribute.Remove
 import weka.filters.unsupervised.instance.RemoveWithValues
 import java.io.File
+import java.io.PrintWriter
 import java.util.*
 
 /**
  * Created by warrior on 04/07/16.
  */
 
-fun main(args: Array<String>) {
-    searchTreeNumber(Class.GENDER)
-    trainSingleModels(Class.GENDER)
-    fullInstancesTrain(Class.GENDER)
-    vote(Class.GENDER)
-    stacking(Class.GENDER)
-    boosting(Class.GENDER)
+var treeSizes: Map<Class, Map<DatasetInfo, Map<Metric, Int>>> = mapOf()
 
-    searchTreeNumber(Class.RELATIONSHIP)
-    trainSingleModels(Class.RELATIONSHIP)
-    fullInstancesTrain(Class.RELATIONSHIP)
-    vote(Class.RELATIONSHIP)
-    stacking(Class.RELATIONSHIP)
-    boosting(Class.RELATIONSHIP)
+fun main(args: Array<String>) {
+    treeSizes = searchTreeSize(Class.values().asList())
+
+    val resultFolder = File("results")
+    if (!resultFolder.exists()) {
+        resultFolder.mkdir()
+    }
+    for (c in listOf(Class.AGE_GROUP, Class.GENDER, Class.RELATIONSHIP, Class.EDUCATION_LEVEL_BINARY, Class.EDUCATION_LEVEL_TERNARY/*, Class.OCCUPATION*/)) {
+        println("---- ${c.name} ----")
+        val results = ArrayList<EvaluationResult>()
+        for (useOversampling in listOf(false, true)) {
+            results += trainSingleModels(c, useOversampling, Metric.values())
+            results += boosting(c, useOversampling, Metric.values())
+            results += stacking(c, useOversampling, Metric.values())
+            results += vote(c, useOversampling, Metric.values())
+        }
+
+        PrintWriter(File(resultFolder, "${c.name}.txt")).use {
+            val resultMap = results.groupBy { it.javaClass }
+            for ((k, v) in resultMap) {
+                val result = v.maxBy { it.value }
+                println(result)
+                it.println(result)
+            }
+        }
+    }
 }
 
+private fun searchTreeSize(classes: List<Class>): Map<Class, Map<DatasetInfo, Map<Metric, Int>>> {
+    val mapper = ObjectMapper()
+    val treeNumbers = HashMap<Class, Map<DatasetInfo, Map<Metric, Int>>>()
+    val treeSizeFolder = File("tree-sizes")
+    if (!treeSizeFolder.exists()) {
+        treeSizeFolder.mkdir()
+    }
+    for (c in classes) {
+        val file = File(treeSizeFolder, "${c.suffix}.json")
+        if (file.exists()) {
+            treeNumbers[c] = mapper.readValue(file, object : TypeReference<Map<DatasetInfo, Map<Metric, Int>>>() {})
+        } else {
+            val generator = mapper.factory.createGenerator(file, JsonEncoding.UTF8)
+            generator.use { g ->
+                g.writeStartObject()
+                val map = searchTreeSize(c, *Metric.values())
+                treeNumbers[c] = map
+                g.writeObject(map)
+                g.flush()
+                g.writeEndObject()
+            }
+        }
+    }
 
-private fun searchTreeNumber(c: Class) {
+    return treeNumbers
+}
+
+private fun searchTreeSize(c: Class, vararg metrics: Metric): Map<DatasetInfo, Map<Metric, Int>> {
+    println("search for $c")
     val train = load("$DATASET_FOLDER/fullTrain${c.suffix}.arff")
-    DatasetInfo.values()
+    return DatasetInfo.values()
             .map { info ->
-                val (num, fMeasure) = (10..150 step 10)
+                val results = (10..150 step 10)
                         .toList()
                         .parallelStream()
                         .map { num ->
                             val classifier = createClassifier(info, randomForest(num))
                             val evaluation = Evaluation(train)
                             evaluation.crossValidateModel(classifier, train, 10, Random())
-                            val fMeasure = evaluation.unweightedMacroFmeasure()
-                            println("${info.datasetName}: $num -> $fMeasure")
-                            Pair(num, fMeasure)
+                            val results = metrics.map {
+                                evaluation.macro(it.fn)
+                            }
+
+                            println("${info.datasetName}: $num ->")
+                            for ((i, m) in metrics.withIndex()) {
+                                println("    ${m.name}: ${results[i]}")
+                            }
+                            Pair(num, results)
                         }
-                        .max({ f, s -> f.second.compareTo(s.second) })
-                        .get()
-                Triple(info.datasetName, num, fMeasure)
+                        .toList()
+
+                val bestResults = DoubleArray(metrics.size) { -1.0 }
+                val bestSizes = IntArray(metrics.size)
+                for ((num, values) in results) {
+                    for ((i, v) in values.withIndex()) {
+                        if (bestResults[i] < v) {
+                            bestResults[i] = v
+                            bestSizes[i] = num
+                        }
+                    }
+                }
+
+                val map = HashMap<Metric, Int>()
+                println("-- ${info.datasetName}. best tree sizes: --")
+                for ((i, m) in metrics.withIndex()) {
+                    map[m] = bestSizes[i]
+                    println("    $m -> ${bestSizes[i]}(${bestResults[i]})")
+                }
+                Pair(info, map)
             }
-            .forEach {
-                val (name, num, fMeasure) = it
-                println("$name. best tree number $num with $fMeasure")
-            }
+            .toMap()
 }
 
-private fun trainSingleModels(c: Class) {
-    val (test, train) = loadDataset(c)
+private fun trainSingleModels(c: Class, useOversampling: Boolean, metrics: Array<Metric>): List<EvaluationResult> {
+    val (train, test) = loadDataset(c, useOversampling)
 
     val modelFolder = "$MODEL_FOLDER/${c.suffix}"
     File(modelFolder).mkdirs()
 
-    Arrays.stream(DatasetInfo.values())
-            .parallel()
-            .map { info ->
-                val forestSize = forestSize(c, info)
-                val classifier = createClassifier(info, randomForest(forestSize))
-                info to classifier
-            }
-            .forEach {
-                val (info, classifier) = it
-                buildAndEvaluate(classifier, train, test, info.datasetName)
-                saveModel(classifier, "$modelFolder/${info.datasetName}.model")
-            }
+    val results = ArrayList<EvaluationResult>()
+    for (m in metrics) {
+        results += Arrays.stream(DatasetInfo.values())
+                .parallel()
+                .map { info ->
+                    val forestSize = treeSizes.get(c)?.get(info)?.get(m)!!
+                    val classifier = createClassifier(info, randomForest(forestSize))
+                    val res = buildAndEvaluate(classifier, train, test, info.datasetName, m)
+                    saveModel(classifier, "$modelFolder/${info.datasetName}-$m.model")
+                    res
+                }
+                .toList()
+                .flatten()
+    }
+    results.forEach { it.useOversampling = useOversampling }
+    return results
 }
 
-private fun fullInstancesTrain(c: Class) {
-    val (train, test) = loadDataset(c)
+private fun vote(c: Class, useOversampling: Boolean, metrics: Array<Metric>): List<EvaluationResult> {
+    return train(c, useOversampling, metrics) { c, m, modelFolder ->
+        val vote = Vote()
+        Arrays.stream(DatasetInfo.values())
+                .forEach { info ->
+                    val classifier = loadModel("$modelFolder/${info.datasetName}-$m.model")
+                    vote.addPreBuiltClassifier(classifier)
+                }
+        "vote" to vote
+    }
+}
 
-    val classifier = RandomForest()
-    classifier.numExecutionSlots = cores()
-    classifier.debug = true
-    buildAndEvaluate(classifier, train, test, "full")
+private fun stacking(c: Class, useOversampling: Boolean, metrics: Array<Metric>,
+                      metaClassifier: Classifier = RandomForest()): List<EvaluationResult> {
+    return train(c, useOversampling, metrics) { c, m, f ->
+        val stacking = Stacking()
+        stacking.metaClassifier = metaClassifier
+        stacking.classifiers = DatasetInfo.values()
+                .map { info ->
+                    val forestSize = treeSizes.get(c)?.get(info)?.get(m)!!
+                    createClassifier(info, randomForest(forestSize))
+                }.toTypedArray()
+        stacking.numExecutionSlots = Math.min(cores(), stacking.classifiers.size)
+        "stacking" to stacking
+    }
+}
+
+private fun boosting(c: Class, useOversampling: Boolean, metrics: Array<Metric>): List<EvaluationResult> {
+    return train(c, useOversampling, metrics) { c, m, f ->
+        val boosting = AdaBoosting()
+        val classifiers = DatasetInfo.values().map { info ->
+            val forestSize = treeSizes.get(c)?.get(info)?.get(m)!!
+            createClassifier(info, randomForest(forestSize, cores()))
+        }
+        boosting.setClassifiers(classifiers)
+        boosting.debug = true
+        "boosting" to boosting
+    }
+}
+
+private fun train(c: Class, useOversampling: Boolean, metrics: Array<Metric>,
+                  block: (c: Class, m: Metric, modelFolder: String) -> Pair<String, Classifier>): List<EvaluationResult> {
+    val (train, test) = loadDataset(c, useOversampling)
 
     val modelFolder = "$MODEL_FOLDER/${c.suffix}"
     File(modelFolder).mkdirs()
-    saveModel(classifier, "$modelFolder/full.model")
+
+    val results = ArrayList<EvaluationResult>()
+    for (m in metrics) {
+        val (name, classifier) = block(c, m, modelFolder)
+        results += buildAndEvaluate(classifier, train, test, name, m)
+        saveModel(classifier, "$modelFolder/$name-$m.model")
+    }
+
+    results.forEach { it.useOversampling = useOversampling }
+    return results
 }
 
-private fun vote(c: Class) {
-    val (train, test) = loadDataset(c)
-
-    val modelFolder = "$MODEL_FOLDER/${c.suffix}"
-    File(modelFolder).mkdirs()
-
-    val vote = Vote()
-
-    Arrays.stream(DatasetInfo.values())
-            .forEach { info ->
-                val classifier = loadModel("$modelFolder/${info.datasetName}.model")
-                vote.addPreBuiltClassifier(classifier)
-            }
-
-    buildAndEvaluate(vote, train, test, "vote")
-    saveModel(vote, "$modelFolder/vote.model")
-}
-
-private fun stacking(c: Class, metaClassifier: Classifier = RandomForest()) {
-    val (train, test) = loadDataset(c)
-
-    val stacking = Stacking()
-    stacking.metaClassifier = metaClassifier
-    stacking.classifiers = DatasetInfo.values()
-            .map { info ->
-                val forestSize = forestSize(c, info)
-                createClassifier(info, randomForest(forestSize))
-            }.toTypedArray()
-    stacking.numExecutionSlots = Math.min(cores(), stacking.classifiers.size)
-
-    stacking.debug = true
-    buildAndEvaluate(stacking, train, test, "stacking")
-
-    val modelFolder = "$MODEL_FOLDER/${c.suffix}"
-    File(modelFolder).mkdirs()
-    saveModel(stacking, "$modelFolder/stacking(${metaClassifier.javaClass.simpleName}).model")
-}
-
-private fun boosting(c: Class) {
-    val (test, train) = loadDataset(c)
-
-    val boosting = AdaBoosting()
-    val classifiers = DatasetInfo.values().map { info ->
-                val forestSize = forestSize(c, info)
-                createClassifier(info, randomForest(forestSize, cores()))
-            }
-    boosting.setClassifiers(classifiers)
-    boosting.debug = true
-    buildAndEvaluate(boosting, train, test, "boosting")
-
-    val modelFolder = "$MODEL_FOLDER/${c.suffix}"
-    File(modelFolder).mkdirs()
-    saveModel(boosting, "$modelFolder/boosting.model")
-}
-
-private fun loadDataset(c: Class): Pair<Instances, Instances> {
-    val train = load("$DATASET_FOLDER/fullTrain${c.suffix}.arff")
+private fun loadDataset(c: Class, useOversampling: Boolean = false): Pair<Instances, Instances> {
+    var train = load("$DATASET_FOLDER/fullTrain${c.suffix}.arff")
+    if (useOversampling) {
+        train = train.oversampling()
+    }
     val test = load("$DATASET_FOLDER/fullTest${c.suffix}.arff")
     return Pair(train, test)
+}
+
+private fun Instances.oversampling(): Instances {
+    val distribution = classDistribution()
+
+    var outInstances = this
+    val mostCommon = distribution.max()!!
+    for ((i, num) in distribution.withIndex()) {
+        if (num != mostCommon) {
+            outInstances = oversampling(outInstances, i, (mostCommon.toDouble() / num - 1) * 100)
+        }
+    }
+
+    outInstances.classDistribution()
+    return outInstances
+}
+
+private fun Instances.classDistribution(): IntArray {
+    val distribution = IntArray(numClasses())
+    for (inst in this) {
+        if (!inst.classIsMissing()) {
+            distribution[inst.classValue().toInt()]++
+        }
+    }
+    println(relationName())
+    for ((i, num) in distribution.withIndex()) {
+        println("${classAttribute().value(i)}: $num")
+    }
+    return distribution
+}
+
+private fun oversampling(instances: Instances, classIndex: Int, percentage: Double): Instances {
+    val smote = SMOTE()
+    smote.classValue = (classIndex + 1).toString()
+    smote.percentage = percentage
+    smote.setInputFormat(instances)
+    return Filter.useFilter(instances, smote)
 }
 
 private fun randomForest(size: Int, threads: Int = 1): RandomForest {
@@ -162,21 +263,38 @@ private fun randomForest(size: Int, threads: Int = 1): RandomForest {
     return classifier
 }
 
-private fun buildAndEvaluate(classifier: Classifier, train: Instances, test: Instances, name: String) {
-    println("--- $name start ---")
+private fun buildAndEvaluate(classifier: Classifier, train: Instances, test: Instances, name: String,
+                             metric: Metric): List<EvaluationResult> {
     classifier.buildClassifier(train)
     val evaluation = Evaluation(test)
     evaluation.evaluateModel(classifier, test)
-    println("--- $name end ---")
-    printEvaluationResult(evaluation, test)
+    val out = StringBuilder("$name\n")
+    out.append(metric.name).append("\n")
+    val results = evaluation.evaluationResults(name, metric, out)
+    println(out.toString())
+    return results
 }
 
-private fun printEvaluationResult(evaluation: Evaluation, test: Instances) {
-    println("accuracy: " + evaluation.pctCorrect())
-    for (i in 0 until test.numClasses()) {
-        println("f-measure for '${test.classAttribute().value(i)}': ${evaluation.fMeasure(i)}")
+private fun Evaluation.evaluationResults(name: String, metric: Metric, out: StringBuilder): List<EvaluationResult> {
+    val results = ArrayList<EvaluationResult>(2)
+    results += EvaluationResult.Accuracy(name, pctCorrect())
+    out.append("  accuracy: ${pctCorrect()}\n")
+    for (i in 0 until header.numClasses()) {
+        val fn = metric.fn
+        out.append("  metric for '${header.classAttribute().value(i)}': ${fn(i)}\n")
     }
-    println("macro f-measure: ${evaluation.unweightedMacroFmeasure()}")
+    results += when (metric) {
+        Metric.F_MEASURE -> EvaluationResult.MacroFMeasure(name, macro(metric.fn))
+        Metric.RECALL -> EvaluationResult.MacroRecall(name, macro(metric.fn))
+    }
+    out.append("  macro metric: ${macro(metric.fn)}\n")
+    return results
+}
+
+private fun Evaluation.macro(metric: Evaluation.(Int) -> Double): Double {
+    return (0 until header.numClasses())
+            .map { metric(it) }
+            .average()
 }
 
 private fun createClassifier(info: DatasetInfo, baseClassifier: Classifier): FilteredClassifier {
@@ -209,9 +327,26 @@ private fun removeMissingValues(): RemoveWithValues {
     return filter
 }
 
-private fun forestSize(c: Class, info: DatasetInfo): Int = when (c) {
-    Class.GENDER -> info.forestSizeGender
-    Class.RELATIONSHIP -> info.forestSizeRelationship
-    // todo: temporary
-    else -> 100
+enum class Metric(val fn: Evaluation.(Int) -> Double) {
+    RECALL(Evaluation::recall),
+    F_MEASURE(Evaluation::fMeasure)
+}
+
+sealed class EvaluationResult(val name: String, val value: Double) {
+
+    var useOversampling: Boolean = false
+
+    class Accuracy(name: String, value: Double): EvaluationResult(name, value)
+    class MacroRecall(name: String, value: Double): EvaluationResult(name, value)
+    class MacroFMeasure(name: String, value: Double): EvaluationResult(name, value)
+
+    private fun metricName(): String = when (this) {
+        is EvaluationResult.Accuracy -> "accuracy"
+        is EvaluationResult.MacroRecall -> "macro recall"
+        is EvaluationResult.MacroFMeasure -> "macro f-measure"
+    }
+
+    override fun toString(): String {
+        return "${metricName()}: $value ($name${if (useOversampling) {"(oversampling)"} else {""}})"
+    }
 }
